9a4af683cc9f2a380c2fee7631a106a9703caa42
Jun He <jhe@cs.wisc.edu>
2017-10-11 12:02:32 -0600

    Add install-java.sh

---
+++ scripts/install-java.sh
@@ -0,0 +1,6 @@
+
+sudo apt-get install software-properties-common python-software-properties
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install oracle-java8-installer
+
