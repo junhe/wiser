20478f4748341b602db1443f014adb5a4e68c700
Jun He <jhe@cs.wisc.edu>
2017-10-12 09:29:25 -0600

    Add setup-lucene-env

---
+++ scripts/setup-lucene-env.sh
@@ -0,0 +1,6 @@
+sudo apt-get install ant
+wget http://mirrors.advancedhosters.com/apache//ant/ivy/2.4.0/apache-ivy-2.4.0-bin.tar.gz
+tar xf apache-ivy-2.4.0-bin.tar.gz
+mkdir -p ~/.ant/lib/
+cp ./apache-ivy-2.4.0/ivy-2.4.0.jar ~/.ant/lib/
+
