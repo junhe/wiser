f5a203c6da00c300d94ecc703000f5dae9c0debe
Jun He <jhe@cs.wisc.edu>
2017-10-15 09:46:49 -0600

    Refactor

--- src/bench_driver/driver.py
+++ src/bench_driver/driver.py
@@ -13,8 +13,22 @@ class ExperimentWikiSmall(Experiment):
 
         self.DOWNLOAD_DIR = "/mnt/ssd/downloads"
         # self.work_dir = "/mnt/sdc1/work3"
-        self.work_dir = "/mnt/ssd/work-2"
 
+        # self.work_dir = "/mnt/ssd/work-small-wiki"
+        # self.download_url = "https://dumps.wikimedia.org/enwiki/latest/enwiki-latest-pages-articles14.xml-p7697599p7744799.bz2"
+        # self.origin_doc_name = "enwiki-latest-pages-articles14.xml-p7697599p7744799"
+        # self.decompress_cmd = "bunzip2 enwiki-latest-pages-articles14.xml-p7697599p7744799.bz2"
+
+        # self.work_dir = "/mnt/ssd/work-medium-wiki"
+        # self.download_url = "https://dumps.wikimedia.org/enwiki/20171001/enwiki-20171001-pages-articles9.xml-p1791081p2336422.bz2"
+        # self.origin_doc_name = "enwiki-20171001-pages-articles9.xml-p1791081p2336422"
+        # self.decompress_cmd = "bunzip2 enwiki-20171001-pages-articles9.xml-p1791081p2336422.bz2"
+
+
+        # self.work_dir = "/mnt/ssd/work-large-wiki"
+        # self.download_url = "https://dumps.wikimedia.org/enwiki/20171001/enwiki-20171001-pages-articles.xml.bz2"
+        # self.origin_doc_name = "enwiki-20171001-pages-articles.xml"
+        # self.decompress_cmd = "bunzip2 enwiki-20171001-pages-articles.xml.bz2"
 
     def conf(self, i):
         return {}
@@ -23,43 +37,17 @@ class ExperimentWikiSmall(Experiment):
         helpers.prepare_dir(self.DOWNLOAD_DIR)
         helpers.prepare_dir(self.work_dir)
 
-        self.download_small_wiki()
-        # self.download_medium_wiki()
-        # self.download_large_wiki()
+        self.download()
         self.create_line_doc()
         self.create_index()
 
-    def download_small_wiki(self):
-        self.download_url = "https://dumps.wikimedia.org/enwiki/latest/enwiki-latest-pages-articles14.xml-p7697599p7744799.bz2"
-        self.origin_doc_name = "enwiki-latest-pages-articles14.xml-p7697599p7744799"
-        self.decompress_cmd = "bunzip2 enwiki-latest-pages-articles14.xml-p7697599p7744799.bz2"
-
+    def download(self):
         with helpers.cd(self.DOWNLOAD_DIR):
             helpers.shcmd("wget -nc {}".format(self.download_url))
 
             self.origin_doc_path = os.path.join(self.DOWNLOAD_DIR, self.origin_doc_name)
             if not os.path.exists(self.origin_doc_name):
                 helpers.shcmd(self.decompress_cmd)
-
-    def download_medium_wiki(self):
-        with helpers.cd(self.DOWNLOAD_DIR):
-            helpers.shcmd("wget -nc https://dumps.wikimedia.org/enwiki/20171001/enwiki-20171001-pages-articles9.xml-p1791081p2336422.bz2")
-
-            self.origin_doc_name = "enwiki-20171001-pages-articles9.xml-p1791081p2336422"
-            self.origin_doc_path = os.path.join(self.DOWNLOAD_DIR, self.origin_doc_name)
-
-            if not os.path.exists(self.origin_doc_name):
-                helpers.shcmd("bunzip2 enwiki-20171001-pages-articles9.xml-p1791081p2336422.bz2")
-
-    def download_large_wiki(self):
-        with helpers.cd(self.DOWNLOAD_DIR):
-            helpers.shcmd("wget -nc https://dumps.wikimedia.org/enwiki/20171001/enwiki-20171001-pages-articles.xml.bz2")
-
-            self.origin_doc_name = "enwiki-20171001-pages-articles.xml"
-            self.origin_doc_path = os.path.join(self.DOWNLOAD_DIR, self.origin_doc_name)
-
-            if not os.path.exists(self.origin_doc_name):
-                helpers.shcmd("bunzip2 enwiki-20171001-pages-articles.xml")
 
     def create_line_doc(self):
         self.line_file_out = os.path.join(self.work_dir, "linedoc")
