cff529e400e9ffdaaf17b0b1d7dab2ed9dd5bfd9
root <root@cp-1.searchengine.spark-pg0.wisc.cloudlab.us>
2017-10-20 23:32:35 -0500

    Add scripts to get some in-mem implementation

---
+++ scripts/get-Indri.sh
@@ -0,0 +1,2 @@
+wget https://phoenixnap.dl.sourceforge.net/project/lemur/lemur/indri-5.11/indri-5.11.tar.gz
+tar xvzf ./indri-5.11.tar.gz
---
+++ scripts/get-Zambezi.sh
@@ -0,0 +1,1 @@
+git clone https://github.com/sherlockwu/Zambezi.git
