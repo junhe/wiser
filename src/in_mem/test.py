66c8dd6be538d94e1edd2fb0b9305cc163fe7aee
Jun He <jhe@cs.wisc.edu>
2017-10-21 07:08:54 -0600

    Add test file

---
+++ src/in_mem/test.py
@@ -0,0 +1,15 @@
+import unittest
+
+from search_engine import *
+
+class TestPostingList(unittest.TestCase):
+    def test(self):
+        pl = PostingList()
+        pl.update_posting(8, {'frequency': 18})
+
+        payload = pl.get_payload_dict(8)
+        self.assertEquals(payload['frequency'], 18)
+
+
+if __name__ == '__main__':
+    unittest.main()
