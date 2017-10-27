6a6f23fe76f0a75645395cf59d2e461e0dbd3b85
Jun He <jhe@cs.wisc.edu>
2017-10-27 13:21:53 -0600

    Move LineDocPool to utils package for easier reuse

--- src/pysrc/in_mem/wiki.py
+++ src/pysrc/in_mem/wiki.py
@@ -8,6 +8,8 @@ import pickle
 from utils.expbase import Experiment
 from .search_engine import *
 from pyreuse import helpers
+from utils.utils import LineDocPool
+
 
 class QueryPool(object):
     def __init__(self, query_path, n):
@@ -37,25 +39,6 @@ class QueryPool(object):
         # format: "hello world"
         return query
 
-
-class LineDocPool(object):
-    def __init__(self, doc_path):
-        self.fd = open(doc_path)
-
-        # Get column names
-        # The first line of the doc must be the header
-        # Sample header:
-        #   FIELDS_HEADER_INDICATOR###      doctitle        docdate body
-        header = self.fd.readline()
-        self.col_names = header.split("###")[1].split()
-
-    def doc_iterator(self):
-        for line in self.fd:
-            yield self.line_to_dict(line)
-
-    def line_to_dict(self, line):
-        items = line.split("\t")
-        return {k:v for k,v in zip(self.col_names, items)}
 
 
 def save_engine(engine, path):
--- src/pysrc/utils/utils.py
+++ src/pysrc/utils/utils.py
@@ -1,5 +1,23 @@
 
-def myutil():
-    print "this is my util"
+class LineDocPool(object):
+    def __init__(self, doc_path):
+        self.fd = open(doc_path)
+
+        # Get column names
+        # The first line of the doc must be the header
+        # Sample header:
+        #   FIELDS_HEADER_INDICATOR###      doctitle        docdate body
+        header = self.fd.readline()
+        self.col_names = header.split("###")[1].split()
+
+    def doc_iterator(self):
+        for line in self.fd:
+            yield self.line_to_dict(line)
+
+    def line_to_dict(self, line):
+        items = line.split("\t")
+        return {k:v for k,v in zip(self.col_names, items)}
+
+
 
 
