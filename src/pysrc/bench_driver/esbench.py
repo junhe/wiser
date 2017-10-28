719d66663c0eb7aba3220a1bde9e898cc728d1f4
Jun He <jhe@cs.wisc.edu>
2017-10-28 15:11:55 -0600

    Add ES benchmark with linedoc

--- src/pysrc/bench_driver/esbench.py
+++ src/pysrc/bench_driver/esbench.py
@@ -1,10 +1,13 @@
 import pprint
+import os
 import datetime
+import time
 
 from elasticsearch import Elasticsearch
 
 from expbase import Experiment
 from utils.utils import LineDocPool, QueryPool
+from pyreuse import helpers
 
 class QueryPoolOLD(object):
     def __init__(self, query_path, n):
@@ -39,7 +42,7 @@ class WikiClient(object):
     def search(self, query_string):
         body = {
             "_source": False,
-            # "size": 0, # setting this to 0  will get 5x speedup
+            "size": 0, # setting this to 0  will get 5x speedup
             "query": {
                 "query_string" : {
                     "fields" : ["body"],
@@ -55,6 +58,13 @@ class WikiClient(object):
 
         return response
 
+    def delete_index(self):
+        ret = self.es_client.indices.delete(index=self.index_name)
+        assert ret['acknowledged'] == True
+
+    def clear_cache(self):
+        ret = self.es_client.indices.clear_cache(index=self.index_name)
+
     def build_index(self, line_doc_path, n_docs):
         line_pool = LineDocPool(line_doc_path)
 
@@ -65,47 +75,65 @@ class WikiClient(object):
                 break
 
             res = self.es_client.index(index=self.index_name, doc_type='articles', id=i, body=d)
-            print res
 
             if i % 100 == 0:
-                print i,
+                print "{}/{}".format(i, n_docs)
         print
 
 
 class ExperimentEs(Experiment):
     def __init__(self):
-        self._n_treatments = 1
-        self._exp_name = "es-experiment"
-
-        exit(1)
+        self._n_treatments = 3
+        self._exp_name = "es-fast"
 
     def conf(self, i):
-        return {'n_query': 1000}
+        return {
+                'doc_count': 10**(i+3),
+                'query_count': 100000}
 
     def before(self):
-        self.client = WikiClient()
-        self.query_pool =  QueryPool("/mnt/ssd/downloads/wiki_QueryLog", 10000)
+        pass
 
     def beforeEach(self, conf):
-        self.start_time = datetime.datetime.now()
+        self.client = WikiClient()
+        self.client.delete_index()
+        self.client.build_index("/mnt/ssd/downloads/linedoc_tokenized", conf['doc_count'])
+        self.client.clear_cache()
+        time.sleep(5)
+
+        self.query_pool =  QueryPool("/mnt/ssd/downloads/wiki_QueryLog", conf['query_count'])
+
+        self.starttime = datetime.datetime.now()
 
     def treatment(self, conf):
-        for i in range(conf['n_query']):
+        for i in range(conf['query_count']):
             query = self.query_pool.next_query()
             response = self.client.search(query)
             # print query
+            # print response['hits']['total']
             # for hit in response['hits']['hits']:
                 # print hit['_source']['title']
                 # print
                 # break
 
     def afterEach(self, conf):
-        self.end_time = datetime.datetime.now()
-        total_queries = conf['n_query']
-        total_secs = (self.end_time - self.start_time).total_seconds()
-        print 'total_queries', total_queries
-        print 'total_secs', total_secs
-        print 'queries_per_sec', total_queries / total_secs
+        self.endtime = datetime.datetime.now()
+        duration = (self.endtime - self.starttime).total_seconds()
+        print "Duration:", duration
+        query_per_sec = conf['query_count'] / duration
+        print "Query per second:", query_per_sec
+        d = {
+            "duration": duration,
+            "query_per_sec": query_per_sec,
+            }
+        d.update(conf)
+
+        perf_path = os.path.join(self._subexpdir, "perf.txt")
+        print 'writing to', perf_path
+        helpers.table_to_file([d], perf_path, width=0)
+
+        config_path = os.path.join(self._subexpdir, "config.json")
+        helpers.shcmd("touch " + config_path)
 
 
 
