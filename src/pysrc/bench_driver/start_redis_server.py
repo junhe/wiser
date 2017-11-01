74f7a45bb211fa814e7b623f59fba414dbd7dea8
Jun He <jhe@cs.wisc.edu>
2017-11-01 10:09:57 -0600

    Add start_redis_server.py

---
+++ src/pysrc/bench_driver/start_redis_server.py
@@ -0,0 +1,29 @@
+from .go_rsbench import SERVER_PATH, REDISEARCH_SO
+import subprocess
+import sys
+
+
+def start_redis(port):
+    cmd = "{server} --port {port} --loadmodule {mod}".format(
+            server=SERVER_PATH, mod=REDISEARCH_SO, port=port)
+    p = subprocess.Popen(cmd, shell = True)
+    return p
+
+
+def main():
+    if len(sys.argv) != 2:
+        print "Usage: please set the number of redis servers to start"
+        exit(1)
+
+    n = int(sys.argv[1])
+    procs = []
+    for i in range(n):
+        procs.append(start_redis(3679 + i))
+
+    for p in procs:
+        p.wait()
+
+
+if __name__ == "__main__":
+    main()
+
