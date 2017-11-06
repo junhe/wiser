e3adc04c6d58e862118d968c2399c3bcf12f19d1
Jun He <jhe@cs.wisc.edu>
2017-11-05 22:18:03 -0700

    Refactor gobench

--- src/pysrc/benchmarks/rs_bench_go.py
+++ src/pysrc/benchmarks/rs_bench_go.py
@@ -6,6 +6,7 @@ from pyreuse import helpers
 
 from utils.utils import LineDocPool, QueryPool
 from utils.expbase import Experiment
+from .es_bench import WikiClient
 
 
 
@@ -25,21 +26,16 @@ def hosts_string(host, n, start_port):
 
 class ExperimentRsbenchGo(Experiment):
     def __init__(self):
-        self._n_treatments = 5
-        self._exp_name = "es-gobench-1shard-obama-001"
+        self._exp_name = "es-gobench-5shards-001"
 
-        self.n_shards = 1
-        self.n_hosts = 1
-        self.host = "http://localhost"
-
-        self.engine = "elastic"
-        # self.engine = "redis"
-
-        if self.engine == "elastic":
-            self.start_port = 9200
-            self.n_hosts = 1
-        elif self.engine == "redis":
-            self.start_port = 6379
+        self.paras = helpers.parameter_combinations({
+                    'worker_count': [1, 16, 32, 64, 128],
+                    'query': ['hello', 'barack obama'],
+                    'engine': ['elastic'],
+                    'n_shards': [5],
+                    'n_hosts': [1]
+                    })
+        self._n_treatments = len(self.paras)
 
 
     def build_index(self, n_shards, n_hosts, engine, start_port):
@@ -49,30 +45,46 @@ class ExperimentRsbenchGo(Experiment):
                     hosts=hosts_string(self.host, n_hosts, start_port), filepath=WIKI_ABSTRACT))
 
     def conf(self, i):
-        n_clients = [1, 16, 32, 64, 128]
-        return {'n_clients': n_clients[i],
+        para = self.paras[i]
+
+        d = {
+                'n_clients': para['worker_count'],
                 'expname': self._exp_name,
-                'n_shards': self.n_shards,
-                'n_hosts': self.n_hosts,
-                'query': 'barack obama',
-                'engine': self.engine,
+                'n_shards': para['n_shards'],
+                'n_hosts': para['n_hosts'],
+                'query': para['query'],
+                'engine': para['engine'],
                 'benchmark': 'GoBench',
                 }
+
+        if para['engine'] == "elastic":
+            es_client = WikiClient("wik")
+            d['n_shards'] = es_client.get_number_of_shards()
+
+        return d
 
     def before(self):
         # self.build_index(self.n_shards, self.n_hosts, self.engine, self.start_port)
         pass
 
     def treatment(self, conf):
+        if conf['engine'] == "elastic":
+            start_port = 9200
+            n_hosts = 1
+            host = "http://localhost"
+        elif conf['engine'] == "redis":
+            start_port = 6379
+            host = "localhost"
+
         helpers.shcmd("{bench_exe} -engine {engine} -shards {n_shards} " \
                 "-hosts \"{hosts}\" "\
                 "-benchmark search -queries \"{query}\" -c {n_clients} " \
                 "-o {outpath}".format(
-                    engine = self.engine,
+                    engine = conf['engine'],
                     bench_exe = BENCH_EXE,
                     n_clients = conf['n_clients'],
-                    hosts = hosts_string(self.host, self.n_hosts, self.start_port),
-                    n_shards = self.n_shards,
+                    hosts = hosts_string(host, conf['n_hosts'], start_port),
+                    n_shards = conf['n_shards'],
                     query = conf['query'],
                     outpath = os.path.join(self._subexpdir, "out.csv")
                     ))
