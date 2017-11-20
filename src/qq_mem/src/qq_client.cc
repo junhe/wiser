e0897c6d89c5cbea75a1df6b03939daeabb49789
Jun He <jhe@cs.wisc.edu>
2017-11-20 11:58:22 -0700

    Change indention to 4 spaces

--- src/qq_mem/src/qq_client.cc
+++ src/qq_mem/src/qq_client.cc
@@ -18,78 +18,78 @@ using qq::AddDocumentRequest;
 using qq::StatusReply;
 
 class QQEngineClient {
- public:
-  QQEngineClient(std::shared_ptr<Channel> channel)
-      : stub_(QQEngine::NewStub(channel)) {}
+    public:
+        QQEngineClient(std::shared_ptr<Channel> channel)
+            : stub_(QQEngine::NewStub(channel)) {}
 
-  // Assembles the client's payload, sends it and presents the response back
-  // from the server.
-  std::string SayHello(const std::string& user) {
-    // Data we are sending to the server.
-    HelloRequest request;
-    request.set_name(user);
+        // Assembles the client's payload, sends it and presents the response back
+        // from the server.
+        std::string SayHello(const std::string& user) {
+            // Data we are sending to the server.
+            HelloRequest request;
+            request.set_name(user);
 
-    // Container for the data we expect from the server.
-    HelloReply reply;
+            // Container for the data we expect from the server.
+            HelloReply reply;
 
-    // Context for the client. It could be used to convey extra information to
-    // the server and/or tweak certain RPC behaviors.
-    ClientContext context;
+            // Context for the client. It could be used to convey extra information to
+            // the server and/or tweak certain RPC behaviors.
+            ClientContext context;
 
-    // The actual RPC.
-    Status status = stub_->SayHello(&context, request, &reply);
+            // The actual RPC.
+            Status status = stub_->SayHello(&context, request, &reply);
 
-    // Act upon its status.
-    if (status.ok()) {
-      return reply.message();
-    } else {
-      std::cout << status.error_code() << ": " << status.error_message()
-                << std::endl;
-      return "RPC failed";
-    }
-  }
+            // Act upon its status.
+            if (status.ok()) {
+                return reply.message();
+            } else {
+                std::cout << status.error_code() << ": " << status.error_message()
+                    << std::endl;
+                return "RPC failed";
+            }
+        }
 
-  std::string AddDocument() {
-    AddDocumentRequest request;
-    request.mutable_document()->set_title("My first document");
-    request.mutable_document()->set_url("http://wikipedia.org");
-    request.mutable_document()->set_body("BIG body");
+        std::string AddDocument() {
+            AddDocumentRequest request;
+            request.mutable_document()->set_title("My first document");
+            request.mutable_document()->set_url("http://wikipedia.org");
+            request.mutable_document()->set_body("BIG body");
 
-    request.mutable_options()->set_save(true);
+            request.mutable_options()->set_save(true);
 
-    StatusReply reply;
+            StatusReply reply;
 
-    ClientContext context;
+            ClientContext context;
 
-    // Here we can the stub's newly available method we just added.
-    Status status = stub_->AddDocument(&context, request,  &reply);
-    if (status.ok()) {
-      return reply.message();
-    } else {
-      std::cout << status.error_code() << ": " << status.error_message()
-                << std::endl;
-      return "RPC failed";
-    }
-  }
+            // Here we can the stub's newly available method we just added.
+            Status status = stub_->AddDocument(&context, request,  &reply);
+            if (status.ok()) {
+                return reply.message();
+            } else {
+                std::cout << status.error_code() << ": " << status.error_message()
+                    << std::endl;
+                return "RPC failed";
+            }
+        }
 
 
- private:
-  std::unique_ptr<QQEngine::Stub> stub_;
+    private:
+        std::unique_ptr<QQEngine::Stub> stub_;
 };
 
 int main(int argc, char** argv) {
-  // Instantiate the client. It requires a channel, out of which the actual RPCs
-  // are created. This channel models a connection to an endpoint (in this case,
-  // localhost at port 50051). We indicate that the channel isn't authenticated
-  // (use of InsecureChannelCredentials()).
-  QQEngineClient qqengine(grpc::CreateChannel(
-      "localhost:50051", grpc::InsecureChannelCredentials()));
-  std::string user("world");
-  std::string reply = qqengine.SayHello(user);
-  std::cout << "QQEngine received: " << reply << std::endl;
+    // Instantiate the client. It requires a channel, out of which the actual RPCs
+    // are created. This channel models a connection to an endpoint (in this case,
+    // localhost at port 50051). We indicate that the channel isn't authenticated
+    // (use of InsecureChannelCredentials()).
+    QQEngineClient qqengine(grpc::CreateChannel(
+                "localhost:50051", grpc::InsecureChannelCredentials()));
+    std::string user("world");
+    std::string reply = qqengine.SayHello(user);
+    std::cout << "QQEngine received: " << reply << std::endl;
 
-  reply = qqengine.AddDocument();
-  std::cout << "Greeter received: " << reply << std::endl;
+    reply = qqengine.AddDocument();
+    std::cout << "Greeter received: " << reply << std::endl;
 
-  return 0;
+    return 0;
 }
--- src/qq_mem/src/qq_server.cc
+++ src/qq_mem/src/qq_server.cc
@@ -21,47 +21,47 @@ using qq::QQEngine;
 
 // Logic and data behind the server's behavior.
 class QQEngineServiceImpl final : public QQEngine::Service {
-  Status SayHello(ServerContext* context, const HelloRequest* request,
-                  HelloReply* reply) override {
-    std::string prefix("Hello ");
-    reply->set_message(prefix + request->name());
-    return Status::OK;
-  }
+    Status SayHello(ServerContext* context, const HelloRequest* request,
+            HelloReply* reply) override {
+        std::string prefix("Hello ");
+        reply->set_message(prefix + request->name());
+        return Status::OK;
+    }
 
-  Status AddDocument(ServerContext* context, const AddDocumentRequest* request,
-                    StatusReply* reply) override {
-    std::cout << "title" << request->document().title() << std::endl;
-    std::cout << "url" << request->document().url() << std::endl;
-    std::cout << "body" << request->document().body() << std::endl;
+    Status AddDocument(ServerContext* context, const AddDocumentRequest* request,
+            StatusReply* reply) override {
+        std::cout << "title" << request->document().title() << std::endl;
+        std::cout << "url" << request->document().url() << std::endl;
+        std::cout << "body" << request->document().body() << std::endl;
 
-    std::string msg("I am from AddDocument() server.");
-    reply->set_message(msg);
-    reply->set_ok(true);
-    return Status::OK;
-  }
+        std::string msg("I am from AddDocument() server.");
+        reply->set_message(msg);
+        reply->set_ok(true);
+        return Status::OK;
+    }
 };
 
 void RunServer() {
-  std::string server_address("0.0.0.0:50051");
-  QQEngineServiceImpl service;
+    std::string server_address("0.0.0.0:50051");
+    QQEngineServiceImpl service;
 
-  ServerBuilder builder;
-  // Listen on the given address without any authentication mechanism.
-  builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
-  // Register "service" as the instance through which we'll communicate with
-  // clients. In this case it corresponds to an *synchronous* service.
-  builder.RegisterService(&service);
-  // Finally assemble the server.
-  std::unique_ptr<Server> server(builder.BuildAndStart());
-  std::cout << "Server listening on " << server_address << std::endl;
+    ServerBuilder builder;
+    // Listen on the given address without any authentication mechanism.
+    builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+    // Register "service" as the instance through which we'll communicate with
+    // clients. In this case it corresponds to an *synchronous* service.
+    builder.RegisterService(&service);
+    // Finally assemble the server.
+    std::unique_ptr<Server> server(builder.BuildAndStart());
+    std::cout << "Server listening on " << server_address << std::endl;
 
-  // Wait for the server to shutdown. Note that some other thread must be
-  // responsible for shutting down the server for this call to ever return.
-  server->Wait();
+    // Wait for the server to shutdown. Note that some other thread must be
+    // responsible for shutting down the server for this call to ever return.
+    server->Wait();
 }
 
 int main(int argc, char** argv) {
-  RunServer();
+    RunServer();
 
-  return 0;
+    return 0;
 }
