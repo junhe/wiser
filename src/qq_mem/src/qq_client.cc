9d49bab3559a3f3d485354a48960af7a2db69d71
Jun He <jhe@cs.wisc.edu>
2017-11-22 21:39:36 -0700

    Split QQEngineClient's interface and implementation

--- src/qq_mem/src/qq_client.cc
+++ src/qq_mem/src/qq_client.cc
@@ -1,80 +1,56 @@
-#include <iostream>
-#include <memory>
-#include <string>
-
-#include <grpc++/grpc++.h>
-
-#include "qq.grpc.pb.h"
-
-using grpc::Channel;
-using grpc::ClientContext;
-using grpc::Status;
-using qq::QQEngine;
-
-// for AddDocument
-using qq::AddDocumentRequest;
-using qq::StatusReply;
-
-// for Search
-using qq::SearchRequest;
-using qq::SearchReply;
+#include "qq_client.h"
 
 
-class QQEngineClient {
-    public:
-        QQEngineClient(std::shared_ptr<Channel> channel)
-            : stub_(QQEngine::NewStub(channel)) {}
+std::string QQEngineClient::AddDocument() {
+    AddDocumentRequest request;
+    request.mutable_document()->set_title("My first document");
+    request.mutable_document()->set_url("http://wikipedia.org");
+    request.mutable_document()->set_body("BIG body");
 
-        std::string AddDocument() {
-            AddDocumentRequest request;
-            request.mutable_document()->set_title("My first document");
-            request.mutable_document()->set_url("http://wikipedia.org");
-            request.mutable_document()->set_body("BIG body");
 
-            request.mutable_options()->set_save(true);
+    request.mutable_options()->set_save(true);
 
-            StatusReply reply;
+    StatusReply reply;
 
-            ClientContext context;
+    ClientContext context;
 
-            // Here we can the stub's newly available method we just added.
-            Status status = stub_->AddDocument(&context, request,  &reply);
-            if (status.ok()) {
-                return reply.message();
-            } else {
-                std::cout << status.error_code() << ": " << status.error_message()
-                    << std::endl;
-                return "RPC failed";
-            }
+    // Here we can the stub's newly available method we just added.
+    Status status = stub_->AddDocument(&context, request,  &reply);
+    if (status.ok()) {
+        return reply.message();
+    } else {
+        std::cout << status.error_code() << ": " << status.error_message()
+            << std::endl;
+        return "RPC failed";
+    }
+}
+
+
+std::string QQEngineClient::Search() {
+    SearchRequest request;
+    SearchReply reply;
+    ClientContext context;
+
+    request.set_term("body");
+
+    // Here we can the stub's newly available method we just added.
+    Status status = stub_->Search(&context, request,  &reply);
+    if (status.ok()) {
+        std::cout << "Search result: ";
+        for (auto id : reply.doc_ids()) {
+            std::cout << id << " ";
         }
+        std::cout << std::endl;
+        std::cout << "----------" << std::endl;
+        return "OK";
+    } else {
+        std::cout << status.error_code() << ": " << status.error_message()
+            << std::endl;
+        return "RPC failed";
+    }
+}
 
-        std::string Search() {
-            SearchRequest request;
-            SearchReply reply;
-            ClientContext context;
 
-            request.set_term("body");
-
-            // Here we can the stub's newly available method we just added.
-            Status status = stub_->Search(&context, request,  &reply);
-            if (status.ok()) {
-                std::cout << "Search result: ";
-                for (auto id : reply.doc_ids()) {
-                    std::cout << id << " ";
-                }
-                std::cout << std::endl;
-                std::cout << "----------" << std::endl;
-                return "OK";
-            } else {
-                std::cout << status.error_code() << ": " << status.error_message()
-                    << std::endl;
-                return "RPC failed";
-            }
-        }
-
-    private:
-        std::unique_ptr<QQEngine::Stub> stub_;
-};
 
 int main(int argc, char** argv) {
     // Instantiate the client. It requires a channel, out of which the actual RPCs
