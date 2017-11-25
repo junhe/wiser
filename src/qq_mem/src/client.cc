b40ff8ea307d7f01ca900b574f0c6b6ea205ffac
Jun He <jhe@cs.wisc.edu>
2017-11-24 22:41:41 -0700

    Add client multi-threading

--- src/qq_mem/src/client.cc
+++ src/qq_mem/src/client.cc
@@ -1,45 +1,51 @@
 #include <chrono>
+#include <thread>
 
 #include "qq_client.h"
 #include "index_creator.h"
 
 
-
-void make_queries(QQEngineClient &qqengine) {
+void make_queries(int n_queries) {
     std::string reply;
-    int n_queries = 10000;
 
-    auto start = std::chrono::system_clock::now();
+    QQEngineClient qqengine(grpc::CreateChannel(
+                "localhost:50051", grpc::InsecureChannelCredentials()));
+
 
     for (int i = 0; i < n_queries; i++) {
         reply = qqengine.Search("hello");
     }
 
-    auto end = std::chrono::system_clock::now();
-
-    std::chrono::duration<double> diff = end - start;
-    std::cout << "Duration: " << diff.count() << " s\n";
-    std::cout << "Query per sec: " << n_queries / diff.count() << "\n";
-
-    std::cout << "Search: " << reply << std::endl;
 }
 
 
 
 int main(int argc, char** argv) {
-    // Instantiate the client. It requires a channel, out of which the actual RPCs
-    // are created. This channel models a connection to an endpoint (in this case,
-    // localhost at port 50051). We indicate that the channel isn't authenticated
-    // (use of InsecureChannelCredentials()).
     QQEngineClient qqengine(grpc::CreateChannel(
                 "localhost:50051", grpc::InsecureChannelCredentials()));
-
 
     // IndexCreator index_creator("src/testdata/tokenized_wiki_abstract_line_doc", qqengine);
     IndexCreator index_creator("/mnt/ssd/downloads/test_doc_tokenized", qqengine);
     index_creator.DoIndex();
 
-    make_queries(qqengine);
+    std::vector<std::thread> threadList;
+
+
+    auto start = std::chrono::system_clock::now();
+    int n_threads = 32;
+    int n_queries = 50000;
+    for(int i = 0; i < n_threads; i++)
+    {
+        threadList.push_back( std::thread(make_queries, n_queries) );
+    }
+    std::for_each(threadList.begin(), threadList.end(), std::mem_fn(&std::thread::join));
+
+    auto end = std::chrono::system_clock::now();
+
+    std::chrono::duration<double> diff = end - start;
+
+    std::cout << "Duration: " << diff.count() << " s\n";
+    std::cout << "Query per sec: " << n_queries * n_threads / diff.count() << "\n";
 
     return 0;
 }
