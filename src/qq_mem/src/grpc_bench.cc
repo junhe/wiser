1f0f24e9355304f87f5647e03f62aff627c9cc47
Jun He <jhe@cs.wisc.edu>
2017-11-26 14:30:36 -0700

    Add grpc benchmark files

---
+++ src/qq_mem/src/grpc_bench.cc
@@ -0,0 +1,52 @@
+#include <chrono>
+#include <thread>
+
+#include "qq_client.h"
+#include "index_creator.h"
+
+
+void make_queries(int n_queries) {
+    std::string reply;
+
+    QQEngineClient qqengine(grpc::CreateChannel(
+                "localhost:50051", grpc::InsecureChannelCredentials()));
+
+
+    for (int i = 0; i < n_queries; i++) {
+        reply = qqengine.Search("hello");
+    }
+
+}
+
+
+
+int main(int argc, char** argv) {
+    QQEngineClient qqengine(grpc::CreateChannel(
+                "localhost:50051", grpc::InsecureChannelCredentials()));
+
+    // IndexCreator index_creator("src/testdata/tokenized_wiki_abstract_line_doc", qqengine);
+    // IndexCreator index_creator("/mnt/ssd/downloads/test_doc_tokenized", qqengine);
+    IndexCreator index_creator("/mnt/ssd/downloads/enwiki-abstract_tokenized.linedoc", qqengine);
+    index_creator.DoIndex();
+
+    std::vector<std::thread> threadList;
+
+
+    auto start = std::chrono::system_clock::now();
+    int n_threads = 16;
+    int n_queries = 50000;
+    for(int i = 0; i < n_threads; i++)
+    {
+        threadList.push_back( std::thread(make_queries, n_queries) );
+    }
+    std::for_each(threadList.begin(), threadList.end(), std::mem_fn(&std::thread::join));
+
+    auto end = std::chrono::system_clock::now();
+
+    std::chrono::duration<double> diff = end - start;
+
+    std::cout << "Duration: " << diff.count() << " s\n";
+    std::cout << "Query per sec: " << n_queries * n_threads / diff.count() << "\n";
+
+    return 0;
+}
