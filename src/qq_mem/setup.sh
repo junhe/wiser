62a9df4eff8606d97944af157f0caed1eeb76d40
Jun He <jhe@cs.wisc.edu>
2017-11-29 23:05:19 -0600

    Add code to setup cmake

--- src/qq_mem/setup.sh
+++ src/qq_mem/setup.sh
@@ -1,3 +1,5 @@
+sudo apt-get install -y curl
+
 # install grpc
 cd /tmp/
 git clone -b $(curl -L https://grpc.io/release) https://github.com/grpc/grpc
@@ -13,4 +15,11 @@ cd third_party/protobuf
 ./configure
 make
 sudo make install
+
+
+# install new cmake
+cd $HOME
+wget https://cmake.org/files/v3.10/cmake-3.10.0-Linux-x86_64.tar.gz
+tar xf cmake-3.10.0-Linux-x86_64.tar.gz
+echo 'export PATH=$HOME/cmake-3.10.0-Linux-x86_64/bin:$PATH' >> $HOME/.bashrc
 
