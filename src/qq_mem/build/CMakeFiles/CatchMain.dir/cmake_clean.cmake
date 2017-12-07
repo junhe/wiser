bf2d25c4d3d1daf89f8c8ffcdb7754fa51a64de9
root <root@cp-1.searchengine.spark-pg0.wisc.cloudlab.us>
2017-12-06 20:02:51 -0600

    Delete build

--- src/qq_mem/build/CMakeFiles/CatchMain.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/CatchMain.dir/catch_main.cc.o"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang CXX)
-  include(CMakeFiles/CatchMain.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
