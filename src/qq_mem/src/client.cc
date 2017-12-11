481431b464e83fa7a09daca427c9af99c52925dc
Jun He <jhe@cs.wisc.edu>
2017-12-11 12:56:52 -0600

    Add index creation in client.cc

--- src/qq_mem/src/client.cc
+++ src/qq_mem/src/client.cc
@@ -22,8 +22,24 @@ void make_queries(int n_queries) {
 
 
 int main(int argc, char** argv) {
-  auto client = CreateAsyncClient("localhost:50051", 64, 100, 1000, 8, 1, 8);
-  client->Wait();
+
+  auto client = CreateSyncClient("localhost:50051");
+
+  // Create index on server side
+  IndexCreator index_creator(
+        "src/testdata/enwiki-abstract_tokenized.linedoc.sample", *client);
+  index_creator.DoIndex();
+
+  // Search synchroniously
+  std::vector<int> doc_ids;
+  bool ret;
+  ret = client->Search("multicellular", doc_ids);
+  assert(ret == true);
+  assert(doc_ids.size() >= 1);
+
+  auto async_client = CreateAsyncClient("localhost:50051", 64, 100, 1000, 32, 1, 8);
+  async_client->Wait();
+  async_client->ShowStats();
   return 0;
 }
 
