e86dabbaa29535e2e4b652169055a3c05706543d
Jun He <jhe@cs.wisc.edu>
2017-12-12 09:07:58 -0600

    Add in proce engine bench and enwiki sample

---
+++ src/qq_mem/src/in_proc_engine_bench.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "qq_engine.h"
+#include "utils.h"
+
+int K = 1000;
+int M = 1000 * K;
+int B = 1000 * M;
+
+int main(int argc, char **argv) {
+  int n_queries = 1 * M;
+  int n_rows = 1 * M;
+  std::vector<int> doc_ids; 
+
+  QQSearchEngine engine;
+  engine.LoadLocalDocuments(
+      "/mnt/ssd/downloads/enwiki-abstract_tokenized.linedoc", n_rows);
+
+  auto start = utils::now();
+  for (int i = 0; i < n_queries; i++) {
+    doc_ids = engine.Search(TermList{"hello"}, SearchOperator::AND);
+    // std::cout << "doc_ids.size()=" << doc_ids.size() << std::endl;
+  }
+  auto end = utils::now();
+  auto duration = utils::duration(start, end);
+
+  std::cout 
+    << "n_queries: " << n_queries << std::endl
+    << "duration: " << duration << std::endl
+    << "QPS: " << n_queries / duration << std::endl;
+}
+
+
