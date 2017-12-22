8ecd1e1cd7958beaf4126cd6cebf6d64d7648e8e
Jun He <jhe@cs.wisc.edu>
2017-12-22 15:35:56 -0600

    Add ranking bench
    I copied code from master to ranking_bench.cc.
    TODO: reuse original code after merge, delete the redundant code in this file

---
+++ src/qq_mem/src/ranking_bench.cc
@@ -0,0 +1,105 @@
+#include <iostream>
+#include <cassert>
+
+#include <glog/logging.h>
+
+#include "intersect.h"
+#include "ranking.h"
+#include "utils.h"
+
+
+typedef std::vector<int> TopDocs;
+typedef std::tuple<int, int> Offset;  //startoffset, endoffset
+typedef std::vector<Offset> Offsets;
+
+class TermWithOffset {
+	public:
+		Term term_;
+		Offsets offsets_;
+
+		TermWithOffset(Term term_in, Offsets offsets_in) 
+			: term_(term_in), offsets_(offsets_in) {} 
+};
+
+typedef std::vector<TermWithOffset> TermWithOffsetList;
+
+
+// Parse offsets from string
+void handle_positions(const std::string& s, Offset& this_position) {
+    std::size_t pos_split = s.find(",");
+    this_position = std::make_tuple(std::stoi(s.substr(0, pos_split)), std::stoi(s.substr(pos_split+1)));
+    // construct position
+    return;
+}
+
+
+void handle_term_offsets(const std::string& s, Offsets& this_term) {
+    std::string buff{""};
+
+    for(auto n:s) {
+        // split by ;
+        if(n != ';') buff+=n; else
+        if(n == ';' && buff != "") {
+            Offset this_position;
+            // split by ,
+            handle_positions(buff, this_position);
+            this_term.push_back(this_position);
+            buff = "";
+        }
+    }
+
+    return;
+}
+
+const std::vector<Offsets> parse_offsets(const std::string& s) {
+    std::vector<Offsets> res;
+    
+    std::string buff{""};
+
+    for(auto n:s)
+    {
+    // split by .
+        if(n != '.') buff+=n; else
+        if(n == '.' && buff != "") {
+            Offsets this_term;
+            handle_term_offsets(buff, this_term);
+            res.push_back(this_term);
+            buff = "";
+        }
+    }
+    return res;
+}
+
+TermWithOffsetList parse_doc(const std::vector<std::string> &items) {
+	auto title = items[0];
+	auto body = items[1];
+	auto tokens = items[2];
+	auto offsets = items[3];
+
+	std::vector<Offsets> offsets_parsed = parse_offsets(offsets);
+	std::vector<std::string> terms = utils::explode(tokens, ' ');
+	TermWithOffsetList terms_with_offset = {};
+
+	for (int i = 0; i < terms.size(); i++) {
+		Offsets tmp_offsets = offsets_parsed[i];
+		TermWithOffset cur_term(terms[i], tmp_offsets);
+		terms_with_offset.push_back(cur_term);
+	}
+
+	return terms_with_offset;
+}
+
+int main(int argc, char** argv) {
+  google::InitGoogleLogging(argv[0]);
+  // FLAGS_logtostderr = 1; // print to stderr instead of file
+  FLAGS_stderrthreshold = 0; 
+  FLAGS_minloglevel = 0; 
+
+  utils::LineDoc linedoc("./src/testdata/line_doc_offset_untracked");
+
+  for (int i = 0; i < 5; i++) {
+    std::vector<std::string> items;
+    linedoc.GetRow(items);
+    auto terms_with_offset = parse_doc(items);
+	}
+}
