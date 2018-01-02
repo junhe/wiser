cd04b3952e7054b0e92cf3a9088e1dba822a6155
Jun He <jhe@cs.wisc.edu>
2018-01-02 10:51:50 -0600

    Add an intersection implementation from internet

--- src/qq_mem/src/intersect_bench.cc
+++ src/qq_mem/src/intersect_bench.cc
@@ -8,13 +8,32 @@
 #include "intersect.h"
 #include "utils.h"
 
+typedef std::vector<int> array_t;
 
-int main(int argc, char** argv) {
-  google::InitGoogleLogging(argv[0]);
-  // FLAGS_logtostderr = 1; // print to stderr instead of file
-  FLAGS_stderrthreshold = 0; 
-  FLAGS_minloglevel = 0; 
+array_t intersect_vec(const array_t &a1, const array_t &a2) {
+}
 
+array_t findIntersection(const array_t &A, const array_t &B) {
+  array_t intersection;
+  int n1 = A.size();
+  int n2 = B.size();
+  int i = 0, j = 0;
+  while (i < n1 && j < n2) {
+    if (A[i] > B[j]) {
+      j++;
+    } else if (B[j] > A[i]) {
+      i++;
+    } else {
+      intersection.push_back(A[i]);
+      i++;
+      j++;
+    }
+  }
+  return intersection;
+}
+
+
+void qq_mem_bench() {
   PostingList_Vec<Posting> pl01("hello");   
   for (int i = 0; i < 1000000; i++) {
     pl01.AddPosting(Posting(i, 1, Positions{28}));
@@ -29,13 +48,36 @@ int main(int argc, char** argv) {
 
   auto start = utils::now();
   std::vector<DocIdType> ret = intersect<Posting>(lists);
-
-  // for (auto d : ret) {
-    // std::cout << d << ",";
-  // }
   auto end = utils::now();
   auto dur = utils::duration(start, end);
 
   assert(ret.size() == 1000000);
-  std::cout << "Took " << dur << " secs" << std::endl;
+  std::cout << "QQ Intersection Took " << dur << " secs" << std::endl;
+}
+
+void leetcode_bench() {
+  array_t a1, a2;
+  for (int i = 0; i < 1000000; i++) {
+    a1.push_back(i);
+    a2.push_back(i);
+  }
+
+  auto start = utils::now();
+  array_t ret2 = findIntersection(a1, a2);
+  auto end = utils::now();
+  auto dur = utils::duration(start, end);
+
+  assert(ret2.size() == 1000000);
+  std::cout << "LeetCode Took " << dur << " secs" << std::endl;
+}
+
+
+int main(int argc, char** argv) {
+  google::InitGoogleLogging(argv[0]);
+  // FLAGS_logtostderr = 1; // print to stderr instead of file
+  FLAGS_stderrthreshold = 0; 
+  FLAGS_minloglevel = 0; 
+
+  qq_mem_bench();
+  leetcode_bench();
 }
