101c7a6b837e9d3b71dd920aa4819624286ecd6d
Jun He <jhe@cs.wisc.edu>
2018-01-03 11:17:36 -0600

    Experiment more cases for intersection

--- src/qq_mem/src/intersect_bench.cc
+++ src/qq_mem/src/intersect_bench.cc
@@ -33,44 +33,80 @@ array_t findIntersection(const array_t &A, const array_t &B) {
 }
 
 
-void qq_mem_bench() {
+utils::ResultRow qq_mem_bench(const int &n_postings) {
+  const int n_repeats = 100;
+  utils::ResultRow row;
+
   PostingList_Vec<Posting> pl01("hello");   
-  for (int i = 0; i < 1000000; i++) {
+  for (int i = 0; i < n_postings; i++) {
     pl01.AddPosting(Posting(i, 1, Positions{28}));
   }
 
   PostingList_Vec<Posting> pl02("world");   
-  for (int i = 0; i < 1000000; i++) {
+  for (int i = 0; i < n_postings; i++) {
     pl02.AddPosting(Posting(i, 1, Positions{28}));
   }
 
   std::vector<const PostingList_Vec<Posting>*> lists{&pl01, &pl02};
 
   auto start = utils::now();
-  std::vector<DocIdType> ret = intersect<Posting>(lists);
+  for (int i = 0; i < n_repeats; i++) {
+    std::vector<DocIdType> ret = intersect<Posting>(lists);
+  }
   auto end = utils::now();
   auto dur = utils::duration(start, end);
 
-  assert(ret.size() == 1000000);
-  std::cout << "QQ Intersection Took " << dur << " secs" << std::endl;
+  assert(ret.size() == n_postings);
+  row["duration"] = std::to_string(dur / n_repeats);
+  row["n_postings"] = std::to_string(n_postings);
+
+  return row;
 }
 
-void leetcode_bench() {
+void qq_mem_bench_suite() {
+  utils::ResultTable table;
+  table.push_back(qq_mem_bench(1000));
+  table.push_back(qq_mem_bench(10000));
+  table.push_back(qq_mem_bench(100000));
+  table.push_back(qq_mem_bench(1000000));
+
+  std::cout << utils::dump_result_table(table);
+}
+
+utils::ResultRow leetcode_bench(const int &n_postings) {
+  const int n_repeats = 100;
+  utils::ResultRow row;
+
   array_t a1, a2;
-  for (int i = 0; i < 1000000; i++) {
+  for (int i = 0; i < n_postings; i++) {
     a1.push_back(i);
     a2.push_back(i);
   }
 
   auto start = utils::now();
-  array_t ret2 = findIntersection(a1, a2);
+  for (int i = 0; i < n_repeats; i++) {
+    array_t ret2 = findIntersection(a1, a2);
+  }
   auto end = utils::now();
   auto dur = utils::duration(start, end);
 
-  assert(ret2.size() == 1000000);
-  std::cout << "LeetCode Took " << dur << " secs" << std::endl;
+  assert(ret2.size() == n_postings);
+
+  row["duration"] = std::to_string(dur / n_repeats);
+  row["n_postings"] = std::to_string(n_postings);
+
+  return row;
 }
 
+void leetcode_bench_suite() {
+  utils::ResultTable table;
+  table.push_back(leetcode_bench(1000));
+  table.push_back(leetcode_bench(10000));
+  table.push_back(leetcode_bench(100000));
+  table.push_back(leetcode_bench(1000000));
+
+  std::cout << utils::dump_result_table(table);
+}
 
 int main(int argc, char** argv) {
   google::InitGoogleLogging(argv[0]);
@@ -78,6 +114,6 @@ int main(int argc, char** argv) {
   FLAGS_stderrthreshold = 0; 
   FLAGS_minloglevel = 0; 
 
-  qq_mem_bench();
-  leetcode_bench();
+  qq_mem_bench_suite();
+  leetcode_bench_suite();
 }
