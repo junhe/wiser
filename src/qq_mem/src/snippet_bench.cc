2d16e0ad1d1a0bf29155c16c7e0d5410e00edfc2
Jun He <jhe@cs.wisc.edu>
2018-01-09 13:16:55 -0600

    Add snippet bench

---
+++ src/qq_mem/src/snippet_bench.cc
@@ -0,0 +1,85 @@
+#include <vector>
+
+#include <glog/logging.h>
+
+#include "types.h"
+#include "utils.h"
+#include "unifiedhighlighter.h"
+
+
+struct DocumentContext {
+  int doc_id;
+  std::string body;
+  OffsetPairs hello_pairs;
+};
+
+
+Snippets highlight_top_k(std::vector<DocumentContext> contexts) {
+  Snippets snippets;
+
+  for (int i = 0; i < contexts.size(); i++) {
+    auto ctx = contexts[i];
+    OffsetsEnums enums = {};
+    enums.push_back(Offset_Iterator(ctx.hello_pairs));
+
+    SimpleHighlighter highlighter;
+    snippets.push_back(highlighter.highlightOffsetsEnums(enums, 2, ctx.body));
+  }
+
+  // std::cout << snippets.size() << std::endl;
+  return snippets;
+}
+
+void bench() {
+	std::vector<std::string> bodies{
+		"hello l.o",
+    "hello film",
+    "golden hello",
+    "hei hello",
+    "websit hello project",
+    "websit hello project",
+    "websit hello project",
+    "websit hello project",
+    "hello from mar",
+    "hello caesar german:halloh"};
+
+  std::vector<DocumentContext> contexts;
+
+  for (int i = 0; i < bodies.size(); i++) {
+    auto body = bodies[i];
+    auto doc_id = i;
+
+    std::map<Term, OffsetPairs> term_offsets = utils::extract_offset_pairs(body);
+
+    DocumentContext ctx;
+    ctx.doc_id = doc_id;
+    ctx.body = body;
+    ctx.hello_pairs = term_offsets["hello"];
+
+    contexts.push_back(ctx);
+  }
+
+  const int repeats = 1000;
+  auto start = utils::now();
+  for (int i = 0; i < repeats; i++) {
+    highlight_top_k(contexts);
+  }
+  auto end = utils::now();
+  auto duration = utils::duration(start, end);
+  
+  std::cout << "Total duration: " << duration << std::endl;
+  std::cout << "Total repeats: " << repeats << std::endl;
+  std::cout << "Duration: " << duration / repeats << std::endl;
+  std::cout << "QPS: " << repeats / duration << std::endl;
+}
+
+int main(int argc, char** argv) {
+  google::InitGoogleLogging(argv[0]);
+  // FLAGS_logtostderr = 1; // print to stderr instead of file
+  FLAGS_stderrthreshold = 0; 
+  FLAGS_minloglevel = 0; 
+
+  bench();
+}
+
+
