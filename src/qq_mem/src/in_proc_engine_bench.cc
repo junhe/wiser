ba0148186f00efe91735685725f40a5a929d6c4a
Jun He <jhe@cs.wisc.edu>
2018-01-13 09:47:16 -0600

    Clean up

--- src/qq_mem/src/in_proc_engine_bench.cc
+++ src/qq_mem/src/in_proc_engine_bench.cc
@@ -18,46 +18,6 @@ const int M = 1000 * K;
 const int B = 1000 * M;
 
 
-void make_queries(QQSearchEngine *engine, int n_millions) {
-  std::vector<int> doc_ids; 
-  for (int i = 0; i < n_millions; i++) {
-    for (int j = 0; j < M; j++) {
-      doc_ids = engine->Search(TermList{"hello"}, SearchOperator::AND);
-      // volatile auto it = engine->Find(Term("hello"));
-      // std::cout << "doc_ids.size()=" << doc_ids.size() << std::endl;
-    }
-  }
-}
-
-void old_engine_bench() {
-  const int n_millions_queries_per_thread = 1;
-  const int n_threads = 32;
-  const int n_rows = 1 * M;
-  std::vector<std::thread> threadList;
-
-  QQSearchEngine engine;
-  engine.LoadLocalDocuments(
-      "/mnt/ssd/downloads/enwiki-abstract_tokenized.linedoc", n_rows);
-
-  auto start = utils::now();
-  for(int i = 0; i < n_threads; i++)
-  {
-    threadList.push_back( 
-        std::thread( make_queries, &engine, n_millions_queries_per_thread) );
-  }
-  std::for_each(threadList.begin(), threadList.end(), std::mem_fn(&std::thread::join));
-
-  auto end = utils::now();
-  auto duration = utils::duration(start, end);
-
-  int total_queries = n_millions_queries_per_thread * n_threads;
-  std::cout 
-    << "n_queries: " << total_queries << std::endl
-    << "duration: " << duration << std::endl
-    << "QPS (millions/sec): " << total_queries / duration << std::endl;
-}
-
-
 std::unique_ptr<QqMemUncompressedEngine> create_engine(const int &step_height, 
     const int &step_width, const int &n_steps) {
   std::unique_ptr<QqMemUncompressedEngine> engine(new QqMemUncompressedEngine);
