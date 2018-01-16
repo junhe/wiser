bdcf4ecaa5ebbfcc312cb057cf01c350d375592b
Jun He <jhe@cs.wisc.edu>
2018-01-16 09:03:06 -0600

    Add SetBool and SetStrinVec

--- src/qq_mem/src/general_config.h
+++ src/qq_mem/src/general_config.h
@@ -2,11 +2,14 @@
 #define GENERAL_CONFIG_H
 
 #include <map>
+#include <vector>
 
 class GeneralConfig {
  private:
+  std::map<std::string, bool> bool_map_;
   std::map<std::string, int> int_map_;
   std::map<std::string, std::string> string_map_;
+  std::map<std::string, std::vector<std::string>> string_vec_map_;
 
  public:
   void SetInt(const std::string key, const int value) {
@@ -23,6 +26,23 @@ class GeneralConfig {
 
   const std::string GetString(const std::string key) {
     return string_map_[key];
+  }
+
+  void SetBool(const std::string key, const bool value) {
+    bool_map_[key] = value;
+  }
+
+  bool GetBool(const std::string key) {
+    return bool_map_[key];
+  }
+
+  void SetStringVec(const std::string key, 
+                    const std::vector<std::string> value) {
+    string_vec_map_[key] = value;
+  }
+
+  std::vector<std::string> GetStringVec(const std::string key) {
+    return string_vec_map_[key];
   }
 };
 
