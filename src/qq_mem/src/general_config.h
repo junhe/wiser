56b2ad9711201f1ffa7342aa7c57e08e2c817777
Jun He <jhe@cs.wisc.edu>
2018-01-23 22:15:28 -0600

    Assert key exists

--- src/qq_mem/src/general_config.h
+++ src/qq_mem/src/general_config.h
@@ -4,6 +4,9 @@
 #include <map>
 #include <vector>
 
+#include <glog/logging.h>
+
+
 class GeneralConfig {
  private:
   std::map<std::string, bool> bool_map_;
@@ -11,12 +14,32 @@ class GeneralConfig {
   std::map<std::string, std::string> string_map_;
   std::map<std::string, std::vector<std::string>> string_vec_map_;
 
+  void AssertKeyExistence(const std::string key) const {
+    if (!HasKey(key)) {
+      LOG(FATAL) << "Key " << key << " does not exist" << std::endl;
+    }
+  }
+
  public:
+  bool HasKey(const std::string key) const {
+    if (
+        bool_map_.count(key) > 0 ||
+        int_map_.count(key) > 0 ||
+        string_map_.count(key) > 0 ||
+        string_vec_map_.count(key) > 0
+       ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
   void SetInt(const std::string key, const int value) {
     int_map_[key] = value;
   }
 
   const int GetInt(const std::string key) const {
+    AssertKeyExistence(key);
     return int_map_.at(key);
   }
 
@@ -25,6 +48,7 @@ class GeneralConfig {
   }
 
   const std::string GetString(const std::string key) const {
+    AssertKeyExistence(key);
     return string_map_.at(key);
   }
 
@@ -33,6 +57,7 @@ class GeneralConfig {
   }
 
   const bool GetBool(const std::string key) const {
+    AssertKeyExistence(key);
     return bool_map_.at(key);
   }
 
@@ -42,6 +67,7 @@ class GeneralConfig {
   }
 
   const std::vector<std::string> GetStringVec(const std::string key) const {
+    AssertKeyExistence(key);
     return string_vec_map_.at(key);
   }
 };
