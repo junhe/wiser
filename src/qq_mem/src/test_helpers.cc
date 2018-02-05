13cf1509140da41adae6722debc3d81fbbcec4a2
Jun He <jhe@cs.wisc.edu>
2018-02-05 12:30:03 -0600

    Add a new test helper

--- src/qq_mem/src/test_helpers.cc
+++ src/qq_mem/src/test_helpers.cc
@@ -3,14 +3,28 @@
 StandardPosting create_posting(DocIdType doc_id, 
                                          int term_freq,
                                          int n_offset_pairs) {
+  return create_posting(doc_id, term_freq, n_offset_pairs, 0);
+}
+
+
+StandardPosting create_posting(DocIdType doc_id, 
+                                         int term_freq,
+                                         int n_offset_pairs,
+                                         int n_positions) {
   OffsetPairs offset_pairs;
   for (int i = 0; i < n_offset_pairs; i++) {
     offset_pairs.push_back(std::make_tuple(i * 2, i * 2 + 1)); 
   }
 
-  StandardPosting posting(doc_id, term_freq, offset_pairs); 
+  Positions positions;
+  for (int i = 0; i < n_positions; i++) {
+    positions.push_back(i);
+  }
+
+  StandardPosting posting(doc_id, term_freq, offset_pairs, positions); 
   return posting;
 }
+
 
 PostingList_Vec<StandardPosting> create_posting_list_vec(int n_postings) {
   PostingList_Vec<StandardPosting> pl("hello");
--- src/qq_mem/src/test_helpers.h
+++ src/qq_mem/src/test_helpers.h
@@ -7,6 +7,10 @@
 StandardPosting create_posting(DocIdType doc_id, 
                                int term_freq, 
                                int n_offset_pairs);
+StandardPosting create_posting(DocIdType doc_id, 
+                               int term_freq, 
+                               int n_offset_pairs,
+                               int n_postings);
 PostingList_Vec<StandardPosting> create_posting_list_vec(int n_postings);
 PostingListStandardVec create_posting_list_standard(int n_postings);
 
