90ad4fc04e754f7640bdeba95bedba4fc2f02be7
Jun He <jhe@cs.wisc.edu>
2018-02-11 11:20:09 -0600

    Add Create Functions with query producer

--- src/qq_mem/src/grpc_client_impl.cc
+++ src/qq_mem/src/grpc_client_impl.cc
@@ -16,6 +16,14 @@ std::unique_ptr<AsyncClient> CreateAsyncClient(const GeneralConfig &config,
   return client;
 }
 
+std::unique_ptr<AsyncClient> CreateAsyncClient(const GeneralConfig &config,
+    std::unique_ptr<TermPoolArray> query_pool_array,
+    std::unique_ptr<QueryProducer> query_producer) 
+{
+  std::unique_ptr<AsyncClient> client(
+      new AsyncClient(config, std::move(query_pool_array), std::move(query_producer)));
+  return client;
+}
 
 std::unique_ptr<SyncUnaryClient> CreateSyncUnaryClient(const GeneralConfig &config,
     std::unique_ptr<TermPoolArray> query_pool_array) 
@@ -25,6 +33,15 @@ std::unique_ptr<SyncUnaryClient> CreateSyncUnaryClient(const GeneralConfig &conf
   return client;
 }
 
+std::unique_ptr<SyncUnaryClient> CreateSyncUnaryClient(const GeneralConfig &config,
+    std::unique_ptr<TermPoolArray> query_pool_array,
+    std::unique_ptr<QueryProducer> query_producer) 
+{
+  std::unique_ptr<SyncUnaryClient> client(
+      new SyncUnaryClient(config, std::move(query_pool_array), 
+        std::move(query_producer)));
+  return client;
+}
 
 std::unique_ptr<QQEngineSyncClient> CreateSyncClient(const std::string &target) {
   std::unique_ptr<QQEngineSyncClient> client(
@@ -57,6 +74,35 @@ std::unique_ptr<Client> CreateClient(const GeneralConfig &config,
   } else if (config.GetString("synchronization") == "ASYNC") {
       std::unique_ptr<Client> client(
           new AsyncClient(config, std::move(query_pool_array)));
+      return client;
+  }
+}
+
+
+std::unique_ptr<Client> CreateClient(const GeneralConfig &config,
+    std::unique_ptr<TermPoolArray> query_pool_array,
+    std::unique_ptr<QueryProducer> query_producer) {
+  auto sync_type = config.GetString("synchronization");
+  if (sync_type == "SYNC") {
+    auto arity = config.GetString("rpc_arity");
+    if (arity == "UNARY") {
+      std::unique_ptr<Client> client(
+          new SyncUnaryClient(config, 
+                              std::move(query_pool_array),
+                              std::move(query_producer) ));
+      return client;
+    } else if (arity == "STREAMING") {
+      std::unique_ptr<Client> client(
+          new SyncStreamingClient(config, 
+                                  std::move(query_pool_array),
+                                  std::move(query_producer) ));
+      return client;
+    }
+  } else if (config.GetString("synchronization") == "ASYNC") {
+      std::unique_ptr<Client> client(
+          new AsyncClient(config, 
+                          std::move(query_pool_array),
+                          std::move(query_producer) ));
       return client;
   }
 }
--- src/qq_mem/src/grpc_client_impl.h
+++ src/qq_mem/src/grpc_client_impl.h
@@ -957,12 +957,24 @@ class SyncUnaryClient: public Client {
 
 
 std::unique_ptr<QQEngineSyncClient> CreateSyncClient(const std::string &target);
+
 std::unique_ptr<AsyncClient> CreateAsyncClient(const GeneralConfig &config,
     std::unique_ptr<TermPoolArray> query_pool_array);
+std::unique_ptr<AsyncClient> CreateAsyncClient(const GeneralConfig &config,
+    std::unique_ptr<TermPoolArray> query_pool_array,
+    std::unique_ptr<QueryProducer> query_producer);
+
 std::unique_ptr<SyncUnaryClient> CreateSyncUnaryClient(const GeneralConfig &config,
     std::unique_ptr<TermPoolArray> query_pool_array);
+std::unique_ptr<SyncUnaryClient> CreateSyncUnaryClient(const GeneralConfig &config,
+    std::unique_ptr<TermPoolArray> query_pool_array,
+    std::unique_ptr<QueryProducer> query_producer);
+
 std::unique_ptr<Client> CreateClient(const GeneralConfig &config,
     std::unique_ptr<TermPoolArray> query_pool_array);
+std::unique_ptr<Client> CreateClient(const GeneralConfig &config,
+    std::unique_ptr<TermPoolArray> query_pool_array,
+    std::unique_ptr<QueryProducer> query_producer);
 
 #endif
 
