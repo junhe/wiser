cd532e5d9ea2f4c58fb6bfb14caaf1848759b043
Jun He <jhe@cs.wisc.edu>
2018-02-11 22:02:27 -0600

    Better format of reply

--- src/qq_mem/src/utils.cc
+++ src/qq_mem/src/utils.cc
@@ -220,8 +220,9 @@ std::string str_qq_search_reply(const qq::SearchReply &reply) {
   std::ostringstream oss;
   oss << "Size: " << reply.entries_size() ;
   for (int i = 0; i < reply.entries_size(); i++) {
-    oss << " doc_id: " << reply.entries(i).doc_id() 
-      << " snippet: " << reply.entries(i).snippet();
+    oss << "(doc_id: " << reply.entries(i).doc_id() 
+      << " snippet: " << reply.entries(i).snippet()
+      << ") ";
   }
   return oss.str(); 
 }
