9d327ee0f07a9de01f2bf144c7a7bb81c77fc182
Jun He <jhe@cs.wisc.edu>
2018-02-18 10:18:17 -0600

    Fix a bug by decoding more
    If you want to decode less, you need to fix the relative methods in the iterator
    to make sure, for example, term frequency is decoded when you need it

--- src/qq_mem/src/posting_list_delta.h
+++ src/qq_mem/src/posting_list_delta.h
@@ -227,7 +227,7 @@ class PostingListDeltaIterator: public PostingListIteratorService {
       } else {
         AdvanceWithoutDecoding();
       }
-      DecodeUntil(1);
+      DecodeUntil(3);
     }
   }
 
