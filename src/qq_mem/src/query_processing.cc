3262edf6a7a63bcd3fb015a3f766963ffa357d5a
Jun He <jhe@cs.wisc.edu>
2018-02-28 17:32:28 -0600

    Use SingleTermQueryProcessor for single term query

--- src/qq_mem/src/query_processing.cc
+++ src/qq_mem/src/query_processing.cc
@@ -123,8 +123,15 @@ namespace qq_search {
                                            const int n_total_docs_in_index,
                                            const int k,
                                            const bool is_phase) {
-    QueryProcessor qp(pl_iterators, doc_lengths, n_total_docs_in_index, k, is_phase);
-    return qp.Process();
+    if (pl_iterators->size() == 1) {
+      SingleTermQueryProcessor qp(pl_iterators, doc_lengths, 
+          n_total_docs_in_index, k);
+      return qp.Process();
+    } else {
+      QueryProcessor qp(pl_iterators, doc_lengths, 
+          n_total_docs_in_index, k, is_phase);
+      return qp.Process();
+    }
   }
 }
 
