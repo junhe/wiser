2df948562364387da17e974d024d34a5fb215a1a
Jun He <jhe@cs.wisc.edu>
2018-03-03 22:20:38 -0600

    Remove IntersectionResult

--- src/qq_mem/src/tests_2.cc
+++ src/qq_mem/src/tests_2.cc
@@ -90,53 +90,6 @@ TEST_CASE( "SimpleHighlighter works for some corner cases", "[highlighter]" ) {
 
 
 
-TEST_CASE( "IntersectionResult works", "[intersection]" ) {
-  IntersectionResult res;
-
-  SECTION("Doc count works") {
-    res.SetDocCount("term1", 8);
-    REQUIRE(res.GetDocCount("term1") == 8);
-  }
-
-  SECTION("Basic Posting setting and getting work") {
-    StandardPosting *p0, *p1, *p2;
-    const QqMemPostingService *p;
-
-    res.SetPosting(0, "term1", p0);
-    res.SetPosting(1, "term1", p1);
-    res.SetPosting(2, "term1", p2);
-
-    auto row_it = res.row_cbegin();
-    auto col_it = IntersectionResult::col_cbegin(row_it);
-    p = IntersectionResult::GetPosting(col_it);
-    REQUIRE(IntersectionResult::GetCurDocId(row_it) == 0);
-    REQUIRE(p == p0);
-
-    col_it++;
-    REQUIRE(col_it == IntersectionResult::col_cend(row_it));
-
-
-    row_it++;
-    col_it = IntersectionResult::col_cbegin(row_it);
-    p = IntersectionResult::GetPosting(col_it);
-    REQUIRE(IntersectionResult::GetCurDocId(row_it) == 1);
-    REQUIRE(p == p1);
-
-    row_it++;
-    col_it = IntersectionResult::col_cbegin(row_it);
-    p = IntersectionResult::GetPosting(col_it);
-    REQUIRE(IntersectionResult::GetCurDocId(row_it) == 2);
-    REQUIRE(p == p2);
-
-    SECTION("GetRow()") {
-      const IntersectionResult::row_dict_t *row = res.GetRow(0);
-      auto row_it = res.row_cbegin();  
-      REQUIRE(&row_it->second == row);
-    }
-  }
-}
-
-
 TEST_CASE( "Extract offset pairs from a string", "[utils]" ) {
   SECTION("Adding entry") {
     std::map<Term, OffsetPairs> result;
