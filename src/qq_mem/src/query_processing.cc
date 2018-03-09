f3416811d900e6441637d08287a4dac4b985afd7
Jun He <jhe@cs.wisc.edu>
2018-03-09 00:12:10 -0600

    Remove Old query processors

--- src/qq_mem/src/query_processing.cc
+++ src/qq_mem/src/query_processing.cc
@@ -1,26 +1,6 @@
 #include "query_processing.h"
 
 namespace qq_search {
-std::vector<ResultDocEntry> ProcessQuery(IteratorPointers *pl_iterators, 
-                                         const DocLengthStore &doc_lengths,
-                                         const int n_total_docs_in_index,
-                                         const int k,
-                                         const bool is_phase) {
-  if (pl_iterators->size() == 1) {
-    SingleTermQueryProcessor qp(pl_iterators, doc_lengths, 
-        n_total_docs_in_index, k);
-    return qp.Process();
-  } else if (pl_iterators->size() == 2 && is_phase == false) {
-    TwoTermNonPhraseQueryProcessor qp(pl_iterators, doc_lengths, 
-        n_total_docs_in_index, k);
-    return qp.Process();
-  } else {
-    QueryProcessor qp(pl_iterators, doc_lengths, 
-        n_total_docs_in_index, k, is_phase);
-    return qp.Process();
-  }
-}
-
 
 std::vector<ResultDocEntry> ProcessQueryDelta(
      const Bm25Similarity &similarity,
@@ -44,7 +24,7 @@ std::vector<ResultDocEntry> ProcessQueryDelta(
   }
 }
 
-}
+} // namespace qq_search
 
 
 
