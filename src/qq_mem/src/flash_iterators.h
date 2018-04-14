62741868fc1c2a0134124b791d3a5d514f0e25ae
Jun He <jhe@cs.wisc.edu>
2018-04-14 16:22:02 -0600

    Add DocIdIterator

--- src/qq_mem/src/flash_iterators.h
+++ src/qq_mem/src/flash_iterators.h
@@ -85,8 +85,12 @@ class TermFreqIterator {
 
 class DocIdIterator {
  public:
-  DocIdIterator(const uint8_t *buf, const SkipList &skip_list)
-      : buf_(buf), skip_list_(skip_list), cur_iter_type_(BlobFormat::NONE) {
+  DocIdIterator(const uint8_t *buf, const SkipList &skip_list, 
+      const int num_docids)
+    : buf_(buf), skip_list_(skip_list), 
+      cur_iter_type_(BlobFormat::NONE), num_docids_(num_docids) 
+  {
+    SkipTo(0);     
   }
 
   void SkipTo(int posting_index) {
@@ -112,8 +116,41 @@ class DocIdIterator {
     cur_posting_index_  = posting_index;
   }
 
+  int PostingIndex() const {
+    return cur_posting_index_;
+  }
 
-  // You must call SKipTo() at least once before calling Value()
+  bool IsEnd() const {
+    return cur_posting_index_ == num_docids_;
+  }
+
+  void SkipForward(const uint32_t val) {
+    int blob_to_go = GetBlobIndexToGo(val);
+    int new_index_base = blob_to_go * PACK_SIZE;
+
+    std::cout << "SkipForward: " << val 
+      << " blob_to_go: " << blob_to_go << std::endl;
+
+    assert(blob_to_go >= CurBlobIndex());
+
+    if (blob_to_go != CurBlobIndex()) {
+      SkipTo(blob_to_go * PACK_SIZE); 
+    }
+
+    if (cur_iter_type_ == BlobFormat::VINTS) {
+      vints_iter_.SkipForward(val);
+      cur_posting_index_ = new_index_base + vints_iter_.Index();
+    } else {
+      // Packed Ints
+      pack_ints_iter_.SkipForward(val);
+      cur_posting_index_ = new_index_base + pack_ints_iter_.Index();
+    }
+  }
+
+  void Advance() {
+    SkipTo(PostingIndex() + 1);
+  }
+
   uint32_t Value() const {
     if (cur_iter_type_ == BlobFormat::PACKED_INTS) {
       return pack_ints_iter_.Value();
@@ -123,17 +160,34 @@ class DocIdIterator {
   }
 
  private:
+  // Get index of the blob whose prev_doc_id >= val
+  int GetBlobIndexToGo(const uint32_t val) {
+    int i = CurBlobIndex();
+    const int last_index = LastBlobIndex();
+
+    while (i + 1 <= last_index && skip_list_[i + 1].previous_doc_id < val) {
+      i++;
+    }
+
+    return i;
+  }
+
+  int LastBlobIndex() const {
+    return (num_docids_ - 1) / PACK_SIZE;
+  }
+
   void SetupBlob(int blob_index) {
-    off_t blob_off = skip_list_[blob_index].file_offset_of_tf_bag;
+    off_t blob_off = skip_list_[blob_index].file_offset_of_docid_bag;
+    const uint32_t &prev_doc_id = skip_list_[blob_index].previous_doc_id;
     const uint8_t *blob_buf = buf_ + blob_off;
 
     BlobFormat format = GetBlobFormat(blob_buf);
     cur_iter_type_ = format;
 
     if (format == BlobFormat::PACKED_INTS) {
-      pack_ints_iter_.Reset(blob_buf);
+      pack_ints_iter_.Reset(blob_buf, prev_doc_id);
     } else {
-      vints_iter_.Reset(blob_buf);
+      vints_iter_.Reset(blob_buf, prev_doc_id);
     }
   }
 
@@ -147,9 +201,10 @@ class DocIdIterator {
 
   int cur_posting_index_ = 0;
   BlobFormat cur_iter_type_;
+  const int num_docids_;
   
-  PackedIntsIterator pack_ints_iter_;
-  VIntsIterator vints_iter_;
+  DeltaEncodedPackedIntsIterator pack_ints_iter_;
+  DeltaEncodedVIntsIterator vints_iter_;
 };
 
 #endif
--- src/qq_mem/src/tests_11.cc
+++ src/qq_mem/src/tests_11.cc
@@ -251,11 +251,12 @@ TEST_CASE( "TermFreqIterator", "[qqflash][tf_iter]" ) {
 }
 
 
-TEST_CASE( "Doc id iterator", "[qqflash]" ) {
+TEST_CASE( "Doc id iterator", "[qqflash][docid]" ) {
   SECTION("Simple") {
     std::vector<uint32_t> doc_ids;
-    for (uint32_t i = 0; i < 10; i++) {
-      doc_ids.push_back(i * 13);
+    int num_docids = 200;
+    for (uint32_t i = 0; i < num_docids; i++) {
+      doc_ids.push_back(i);
     }
 
     std::string path = "/tmp/tmp.docid";
@@ -268,15 +269,119 @@ TEST_CASE( "Doc id iterator", "[qqflash]" ) {
     utils::FileMap file_map(path);
 
     // Read data by TermFreqIterator
-    DocIdIterator iter((const uint8_t *)file_map.Addr(), skip_list);
+    DocIdIterator iter((const uint8_t *)file_map.Addr(), skip_list, num_docids);
 
     SECTION("Skip one by one") {
-      for (uint32_t i = 0; i < 10; i++) {
+      for (uint32_t i = 0; i < num_docids; i++) {
         iter.SkipTo(i);
-        // REQUIRE(iter.Value() == i * 13);
+        // std::cout << "i: " << iter.Value() << std::endl;
+        REQUIRE(iter.Value() == i);
       }
 
       file_map.Close();
+    }
+
+    SECTION("Advance()") {
+      for (uint32_t i = 0; i < num_docids; i++) {
+        // std::cout << "i: " << iter.Value() << std::endl;
+        REQUIRE(iter.Value() == i);
+        REQUIRE(iter.IsEnd() == false);
+        iter.Advance();
+      }
+      REQUIRE(iter.IsEnd() == true);
+
+      file_map.Close();
+    }
+
+    SECTION("SkipForward() Simple") {
+      REQUIRE(iter.Value() == 0);
+      REQUIRE(iter.PostingIndex() == 0);
+
+      iter.SkipForward(10);
+      REQUIRE(iter.Value() == 10);
+      REQUIRE(iter.PostingIndex() == 10);
+    }
+
+    SECTION("SkipForward() backwards ") {
+      REQUIRE(iter.Value() == 0);
+
+      iter.SkipForward(10);
+      REQUIRE(iter.Value() == 10);
+
+      iter.SkipForward(8);
+      REQUIRE(iter.Value() == 10);
+      REQUIRE(iter.PostingIndex() == 10);
+    }
+
+    SECTION("SkipForward() into vints ") {
+      REQUIRE(iter.Value() == 0);
+
+      iter.SkipForward(127); // end of pack
+      REQUIRE(iter.Value() == 127);
+      REQUIRE(iter.PostingIndex() == 127);
+
+      iter.SkipForward(128); // start of vints
+      REQUIRE(iter.Value() == 128);
+      REQUIRE(iter.PostingIndex() == 128);
+
+      iter.SkipForward(150); // start of vints
+      REQUIRE(iter.Value() == 150);
+      REQUIRE(iter.PostingIndex() == 150);
+    }
+
+    SECTION("SkipForward() beyond the end") {
+      REQUIRE(iter.Value() == 0);
+
+      iter.SkipForward(199); // start of vints
+      REQUIRE(iter.Value() == 199);
+      REQUIRE(iter.IsEnd() == false);
+
+      iter.SkipForward(10000); // start of vints
+      REQUIRE(iter.IsEnd() == true);
+    }
+  }
+
+  SECTION("Large number of sequntial numbers") {
+    std::vector<uint32_t> doc_ids;
+    int num_docids = PACK_SIZE * 10 + 20;
+    for (uint32_t i = 0; i < num_docids; i++) {
+      doc_ids.push_back(i);
+    }
+
+    std::string path = "/tmp/tmp.docid";
+    FileOffsetsOfBlobs file_offsets = DumpCozyBox(doc_ids, path, true);
+
+    SkipList skip_list = CreateSkipListForDodId(
+        GetSkipPostingPreDocIds(doc_ids), file_offsets.BlobOffsets());
+
+    // Open the file
+    utils::FileMap file_map(path);
+
+    // Read data by TermFreqIterator
+    DocIdIterator iter((const uint8_t *)file_map.Addr(), skip_list, num_docids);
+
+    SECTION("Numbers can be retrieved") {
+      for (uint32_t i = 0; i < num_docids; i++) {
+        REQUIRE(iter.Value() == i);
+        REQUIRE(iter.PostingIndex() == i);
+        REQUIRE(iter.IsEnd() == false);
+        iter.Advance();
+      }
+      REQUIRE(iter.IsEnd() == true);
+
+      file_map.Close();
+    }
+
+    SECTION("Go beyond the end") {
+      iter.SkipForward(num_docids + 10);
+      REQUIRE(iter.IsEnd() == true);
+    }
+
+    SECTION("To the last number") {
+      iter.SkipForward(num_docids -1);
+      REQUIRE(iter.Value() == num_docids -1);
+      REQUIRE(iter.PostingIndex() == num_docids -1);
+      REQUIRE(iter.IsEnd() == false);
     }
   }
 }
