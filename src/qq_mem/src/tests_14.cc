70d36809ae9e6e7e41d34edb88269b8cdde7576a
Jun He <jhe@cs.wisc.edu>
2018-04-16 14:06:36 -0600

    Fix a wrong test

--- src/qq_mem/src/tests_14.cc
+++ src/qq_mem/src/tests_14.cc
@@ -5,22 +5,6 @@
 #include "utils.h"
 
 
-TEST_CASE( "Dumping 5-word Engine", "[qqflash][dump1]" ) {
-  FlashEngineDumper engine("/tmp");
-  REQUIRE(engine.TermCount() == 0);
-  engine.LoadLocalDocuments("src/testdata/line_doc_with_positions", 10000, 
-      "WITH_POSITIONS");
-
-  SECTION("Check loading") {
-    REQUIRE(engine.TermCount() == 0);
-  }
-
-  SECTION("Dumping inverted index") {
-    engine.DumpInvertedIndex();
-  }
-}
-
-
 TEST_CASE( "Dumping 1-word Engine", "[qqflash][dump0]" ) {
   std::string dir_path = "/tmp/1-word-engine";
   utils::PrepareDir(dir_path);
@@ -35,6 +19,19 @@ TEST_CASE( "Dumping 1-word Engine", "[qqflash][dump0]" ) {
   }
 }
 
+TEST_CASE( "Dumping 5-word Engine", "[qqflash][dump1]" ) {
+  FlashEngineDumper engine("/tmp");
+  REQUIRE(engine.TermCount() == 0);
+  engine.LoadLocalDocuments("src/testdata/line_doc_with_positions", 10000, 
+      "WITH_POSITIONS");
 
+  SECTION("Check loading") {
+    REQUIRE(engine.TermCount() > 0);
+  }
+
+  SECTION("Dumping inverted index") {
+    engine.DumpInvertedIndex();
+  }
+}
 
 
