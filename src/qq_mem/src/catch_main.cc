804615a6e5caabc5430ce7bc80be1b486457d7dc
Jun He <jhe@cs.wisc.edu>
2018-04-17 06:47:49 -0500

    Set log level

--- src/qq_mem/src/catch_main.cc
+++ src/qq_mem/src/catch_main.cc
@@ -7,7 +7,7 @@ int main( int argc, char* argv[] ) {
   // global setup...
   google::InitGoogleLogging(argv[0]);
   FLAGS_logtostderr = 1; // print to stderr instead of file
-  FLAGS_minloglevel = 3; 
+  FLAGS_minloglevel = 0; 
 
   int result = Catch::Session().run( argc, argv );
 
