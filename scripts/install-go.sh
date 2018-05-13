afa436e014d5c4fa76bd400878c2ca17e0293082
jhe <jhe@node-1.conanwisc.fsperfatscale-pg0.wisc.cloudlab.us>
2018-05-13 16:57:23 -0500

    Add more setup for go client

--- scripts/install-go.sh
+++ scripts/install-go.sh
@@ -3,6 +3,6 @@ tar -C /usr/local -xzf go1.9.2.linux-amd64.tar.gz
 echo "export GOPATH=$HOME/go" >> $HOME/.bashrc
 echo export PATH=/usr/local/go/bin:\$PATH >> ~/.bashrc
 echo export PATH=$GOPATH/bin/:\$PATH >> ~/.bashrc
-
+mkdir $HOME/go
 echo Now execute "source ~/.bashrc"
 
--- scripts/setup-redisearchbench.sh
+++ scripts/setup-redisearchbench.sh
@@ -1,4 +1,8 @@
 cd $GOPATH
 go get github.com/RedisLabs/RediSearchBenchmark
-
+cd $GOPATH/src/github.com/RedisLabs/
+rm -rf RediSearchBenchmark
+git clone git@github.com:junhe/RediSearchBenchmark.git
+cd RediSearchBenchmark
+go install
 
