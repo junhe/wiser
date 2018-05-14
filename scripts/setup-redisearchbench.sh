9ead48697cdff937a5fb6dbd405d5b4074680d8c
Jun He <jhe@cs.wisc.edu>
2018-05-13 21:37:33 -0500

    Add more setup for redisearch bench

--- scripts/setup-redisearchbench.sh
+++ scripts/setup-redisearchbench.sh
@@ -1,4 +1,11 @@
 cd $GOPATH
 go get github.com/RedisLabs/RediSearchBenchmark
+cd $GOPATH/src/github.com/RedisLabs/
+rm -rf RediSearchBenchmark
+git clone git@github.com:junhe/RediSearchBenchmark.git
+cd RediSearchBenchmark
+go install
+
+
 
 
