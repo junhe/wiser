10adb8df3b8fb4949d588fe343c641177523412d
Jun He <jhe@cs.wisc.edu>
2018-05-22 09:32:19 -0600

    Add analyze_window()

--- src/qq_mem/tools/fast_window.py
+++ src/qq_mem/tools/fast_window.py
@@ -32,5 +32,33 @@ class Window(object):
     def has(self, key):
         return self.set.has(key)
 
+    def size(self):
+        return len(self.list)
 
+
+def analyze_window(sequence, win_size):
+    in_win = 0
+    total = 0
+    window = Window()
+
+    for item in sequence:
+        if window.size() < win_size:
+            window.append(item)
+            continue
+
+        if window.has(item):
+            in_win += 1
+        total += 1
+
+        window.pop()
+        window.append(item)
+
+    d = {
+            'window size': win_size,
+            'in_win': in_win,
+            'total': total,
+            'seq_size': len(sequence),
+            'ratio': in_win * 1.0 / total
+        }
+    return d
 
