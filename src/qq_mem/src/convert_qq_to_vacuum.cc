04ae1b36a8fe5d7b695881942bff2f4f5093635f
Jun He <jhe@cs.wisc.edu>
2018-05-23 14:50:38 -0600

    Use gflags in convert

--- src/qq_mem/src/convert_qq_to_vacuum.cc
+++ src/qq_mem/src/convert_qq_to_vacuum.cc
@@ -2,25 +2,45 @@
 #include "vacuum_engine.h"
 
 
+DEFINE_string(qqdump_dir_path, "", "path of the qq dump dir to read from");
+DEFINE_string(vacuum_dir_path, "", "path of the vacuum to write to");
+
+
+void CheckArgs() {
+  if (FLAGS_qqdump_dir_path == "") {
+    LOG(FATAL) << "Arg qqdump_dir_path is not set";
+  }
+
+  if (FLAGS_vacuum_dir_path == "") {
+    LOG(FATAL) << "Arg vaccum_dir_path is not set";
+  }
+}
+
+
 void Convert() {
-  std::string dir_path = "/mnt/ssd/vacuum_0511_chunked_doc_store";
-  utils::PrepareDir(dir_path);
-  FlashEngineDumper engine_dumper(dir_path);
+  std::string vacuum_to_path = FLAGS_vacuum_dir_path;
+  utils::PrepareDir(vacuum_to_path);
+
+  FlashEngineDumper engine_dumper(vacuum_to_path);
   assert(engine_dumper.TermCount() == 0);
-  engine_dumper.LoadQqMemDump("/mnt/ssd/big-engine-char-length-04-19");
+
+  engine_dumper.LoadQqMemDump(FLAGS_qqdump_dir_path);
   assert(engine_dumper.TermCount() > 1000000);
 
   std::cout << "Dumping to vacuum format... " << std::endl;
   engine_dumper.Dump();
 }
 
+
 int main(int argc, char **argv) {
   google::InitGoogleLogging(argv[0]);
   FLAGS_logtostderr = 1; // print to stderr instead of file
   FLAGS_minloglevel = 2; 
 
-  std::cout << "9999999999y237927947293492u\n";
+  std::cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
 	gflags::ParseCommandLineFlags(&argc, &argv, true);
+
+  CheckArgs();
 
   Convert();
 }
