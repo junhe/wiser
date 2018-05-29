447ca5972281525a30d3915a8504a33c9783fd94
Jun He <jhe@cs.wisc.edu>
2018-05-29 08:40:00 -0600

    Add trie bench

---
+++ src/qq_mem/src/trie_bench.cc
@@ -0,0 +1,24 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <memory>
+#include <thread>
+#include <vector>
+#include <climits>
+
+
+#include <gperftools/profiler.h>
+#include <glog/logging.h>
+#include <gflags/gflags.h>
+
+int main(int argc, char **argv) {
+  google::InitGoogleLogging(argv[0]);
+  FLAGS_logtostderr = 1; // print to stderr instead of file
+  FLAGS_minloglevel = 3; 
+
+	gflags::ParseCommandLineFlags(&argc, &argv, true);
+
+  std::cout << "hello" << std::endl;
+}
+
+
