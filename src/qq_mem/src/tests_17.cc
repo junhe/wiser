83031f34fd3adde3a88c6f8a3c8f8b02a2bdf55f
Jun He <jhe@cs.wisc.edu>
2018-06-02 12:14:28 -0600

    Add extended tests

--- src/qq_mem/src/tests_17.cc
+++ src/qq_mem/src/tests_17.cc
@@ -6,6 +6,7 @@
 
 
 typedef std::pair<std::set<std::string>, std::set<std::string>> set_pair ;
+typedef std::pair<std::string, std::string> string_pair;
 
 set_pair GetRandSets(std::size_t n) {
   std::set<std::string> in, out;
@@ -22,6 +23,42 @@ set_pair GetRandSets(std::size_t n) {
 
   return set_pair(in, out);
 }
+
+
+std::string Concat(std::set<std::string> set) {
+  std::string ret;
+
+  std::size_t i = 0;
+  for (auto &s : set) {
+    ret += s;
+
+    if (i != (set.size() - 1)) {
+      ret += " "; 
+    }
+
+    i++;
+  }
+
+  return ret;
+}
+
+
+string_pair GetRandSetStr(std::size_t n) {
+  std::set<std::string> in, out;
+  
+  while (in.size() < n) {
+    in.insert(std::to_string(rand()));
+  }
+
+  while (out.size() < n) {
+    std::string s = std::to_string(rand());
+    if (out.count(s) == 0)
+      out.insert(s);
+  }
+
+  return string_pair(Concat(in), Concat(out));
+}
+
 
 void CheckInitializedFilter(Bloom *blm, const set_pair &setpair) {
   for (auto &s : setpair.first) {
@@ -147,6 +184,98 @@ TEST_CASE( "Bloom filter store", "[bloomfilter]" ) {
 }
 
 
+struct Row {
+  Row(std::string t, std::string p, std::string o)
+    :token(t), phrase_ends(p), outs(o) {}
+
+  std::string ToStr() const {
+    return "token: " + token + "\n" + \
+      "ends: " + phrase_ends + "\n" + \
+      "outs: " + outs + "\n";
+  }
+
+  std::string token;
+  std::string phrase_ends;
+  std::string outs;
+};
+
+struct Table {
+  std::vector<Row> rows;
+  
+  std::vector<std::string> ColTokens() {
+    std::vector<std::string> tokens;
+    for (auto &row : rows) {
+      tokens.push_back(row.token);
+    }
+
+    return tokens;
+  }
+
+  std::vector<std::string> ColEnds() {
+    std::vector<std::string> vec;
+    for (auto &row : rows) {
+      vec.push_back(row.phrase_ends);
+    }
+
+    return vec;
+  }
+
+  std::vector<std::string> ColOuts() {
+    std::vector<std::string> vec;
+    for (auto &row : rows) {
+      vec.push_back(row.outs);
+    }
+
+    return vec;
+  }
+};
+
+void CheckStore(BloomFilterStore &store, Table &table) {
+  for (auto &row : table.rows) {
+    BloomFilterCases cases = store.Lookup(row.token); 
+    REQUIRE(cases.Size() == 1);
+    REQUIRE(cases[0].doc_id == 888);
+
+    // Check real ends
+    std::vector<std::string> real_ends = utils::explode(
+        row.phrase_ends, ' ');
+    for (auto &real_end : real_ends) {
+      REQUIRE(cases[0].blm.Check(real_end) == BLM_MAY_PRESENT);
+    }
+
+    // check terms that are not in filter
+    std::vector<std::string> outs = utils::explode(
+        row.outs, ' ');
+    for (auto &out : outs) {
+      REQUIRE(cases[0].blm.Check(out) == BLM_NOT_PRESENT);
+    }
+  }
+}
+
+TEST_CASE( "Bloom filter serialization", "[bloomfilter]" ) {
+  Table table;
+
+  for (int i = 0; i < 10; i++) {
+    string_pair pair = GetRandSetStr(i);
+    Row row(std::to_string(i), pair.first, pair.second); 
+    table.rows.push_back(row);
+  }
+
+  SECTION("All tokens belong to the same the doc") {
+    BloomFilterStore store(0.00001);
+    
+    store.Add(888, table.ColTokens(), table.ColEnds());
+
+    CheckStore(store, table);
+
+    store.Serialize("/tmp/tmp.store");
+
+    BloomFilterStore store2(0.00001);
+    store2.Deserialize("/tmp/tmp.store");
+
+    CheckStore(store2, table);
+  }
+}
 
 
 
