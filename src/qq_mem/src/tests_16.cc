65436bf13d4b8f940576c838b59d3be350f67f59
Jun He <jhe@cs.wisc.edu>
2018-06-01 22:15:13 -0600

    Move tests

--- src/qq_mem/src/tests_16.cc
+++ src/qq_mem/src/tests_16.cc
@@ -326,68 +326,6 @@ TEST_CASE( "Bloom Filter", "[bloomfilter]" ) {
 }
 
 
-typedef std::pair<std::set<std::string>, std::set<std::string>> set_pair ;
-
-
-set_pair GetRandSets(std::size_t n) {
-  std::set<std::string> in, out;
-  
-  while (in.size() < n) {
-    in.insert(std::to_string(rand()));
-  }
-
-  while (out.size() < n) {
-    std::string s = std::to_string(rand());
-    if (out.count(s) == 0)
-      out.insert(s);
-  }
-
-  return set_pair(in, out);
-}
-
-void CheckInitializedFilter(Bloom *blm, const set_pair &setpair) {
-  for (auto &s : setpair.first) {
-    REQUIRE(CheckBloom(blm, s) == BLM_MAY_PRESENT);
-  }
-
-  for (auto &s : setpair.second) {
-    REQUIRE(CheckBloom(blm, s) == BLM_NOT_PRESENT);
-  }
-}
-
-void CheckBloomFilter(std::size_t n) {
-  float ratio = 0.00001;
-  set_pair setpair = GetRandSets(n);
-  
-  std::vector<std::string> vec;
-  for (auto &s : setpair.first) {
-    vec.push_back(s);
-  }
-
-  Bloom bloom = CreateBloom(ratio, vec);
-
-  CheckInitializedFilter(&bloom, setpair);
-  
-	// Load and check
-  Bloom bloom_test;
-  bloom_set(&bloom_test, n, ratio, bloom.bf);
-
-  CheckInitializedFilter(&bloom_test, setpair);
-  FreeBloom(&bloom);
-}
-
-
-TEST_CASE( "Bloom Filter, extended tests", "[bloomfilter]" ) {
-  srand(1);
-	CheckBloomFilter(1);
-	CheckBloomFilter(2);
-	CheckBloomFilter(3);
-
-	CheckBloomFilter(10);
-	CheckBloomFilter(100);
-}
-
-
 TEST_CASE( "Check string", "[utils]" ) {
   REQUIRE(utils::StartsWith("a", "a") == true);
   REQUIRE(utils::StartsWith("ab", "a") == true);
