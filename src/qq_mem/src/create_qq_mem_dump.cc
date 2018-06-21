259624ac2487cd5e083072ad460842eb3219e453
Jun He <jhe@cs.wisc.edu>
2018-06-21 11:03:33 -0500

    Allow setting dump type

--- src/qq_mem/src/create_qq_mem_dump.cc
+++ src/qq_mem/src/create_qq_mem_dump.cc
@@ -14,6 +14,7 @@ DEFINE_string(dump_dir_path, "", "path of the directory to dump to");
 DEFINE_int32(bloom_entries, 5, "number of entries in a bloom filter");
 DEFINE_double(bloom_ratio, 0.001, "false positive ratio in a bloom filter");
 DEFINE_int32(n_lines, 1, "number of lines to read from line doc");
+DEFINE_string(dump_type, "", "inverted/bloom_begin/bloom_end");
 
 void CheckArgs() {
   if (FLAGS_line_doc_path == "") {
@@ -35,24 +36,22 @@ int main(int argc, char **argv) {
 
   CheckArgs();
 
-  {
+  if (FLAGS_dump_type == "inverted") {
     std::unique_ptr<SearchEngineServiceNew> engine = CreateSearchEngine(
         "qq_mem_compressed");
     engine->LoadLocalDocuments(FLAGS_line_doc_path, FLAGS_n_lines, "WITH_POSITIONS");
     engine->Serialize(FLAGS_dump_dir_path);
     engine.release();
-  }
-
-  {
+  } else if (FLAGS_dump_type == "bloom_begin") {
     BloomBeginDumper bloom_dumper(FLAGS_bloom_ratio, FLAGS_bloom_entries);
     bloom_dumper.Load(FLAGS_line_doc_path, FLAGS_n_lines);
     bloom_dumper.Dump(FLAGS_dump_dir_path);
-  }
-
-  {
+  } else if (FLAGS_dump_type == "bloom_end") {
     BloomEndDumper bloom_dumper(FLAGS_bloom_ratio, FLAGS_bloom_entries);
     bloom_dumper.Load(FLAGS_line_doc_path, FLAGS_n_lines);
     bloom_dumper.Dump(FLAGS_dump_dir_path);
+  } else {
+    LOG(FATAL) << "dump_type " << FLAGS_dump_type << " is not supported";
   }
 }
 
